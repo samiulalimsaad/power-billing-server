6127b1f707b8b500327342fcd3f97208528d9035
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-29 15:59:12 +0600

    registration &  login route added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import { LoginService } from "./Services/Login.Service.js";
+import { RegistrationService } from "./Services/Registration.Service.js";
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res) => {
+    res.status(200).json({ success: true, message: "OK" });
+});
+
+app.post("/api/registration", RegistrationService);
+
+app.post("/api/login", LoginService);
 
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
