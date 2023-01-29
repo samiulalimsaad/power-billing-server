0f742000dcf4bca8df1cba15f19d858b59e950cd
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-29 17:17:25 +0600

    ValidationSchema updated

--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -1,19 +1,27 @@
 import * as Yup from "yup";
 
+const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+const phoneNumberValidationSchema = Yup.string()
+    .required("Phone Number is required")
+    .matches(phoneRegExp, "Phone number is not valid")
+    .min(11, "Phone number too short")
+    .max(11, "Phone number too long");
+
+const emailValidationSchema = Yup.string()
+    .email("Enter a valid email")
+    .required("Email is required");
+
 export const loginValidationSchema = Yup.object({
-    email: Yup.string()
-        .email("Enter a valid email")
-        .required("Name is required")
-        .trim(),
+    email: emailValidationSchema,
     password: Yup.string().required("Password is required").min(8),
 });
 
 export const signUpValidationSchema = Yup.object({
     fullName: Yup.string().required("Name is required"),
-    phone: Yup.string().required("Phone Number is required"),
-    email: Yup.string()
-        .email("Enter a valid email")
-        .required("Email is required"),
+    phone: phoneNumberValidationSchema,
+    email: emailValidationSchema,
     password: Yup.string()
         .min(8, "Password must contain at least 8 characters")
         .required("Password required"),
@@ -21,9 +29,7 @@ export const signUpValidationSchema = Yup.object({
 
 export const billValidationSchema = Yup.object({
     fullName: Yup.string().required("Full Name is required"),
-    email: Yup.string()
-        .email("Enter a valid email")
-        .required("Email is required"),
-    phone: Yup.string().required("Phone Number is required"),
+    email: emailValidationSchema,
+    phone: phoneNumberValidationSchema,
     paidAmount: Yup.number().required("Paid Amount is required"),
 });
