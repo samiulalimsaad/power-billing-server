60d2bed1a7ac9646e14c3b944b5e87a92513684c
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-29 15:43:07 +0600

    validation schema added

--- Models/User.Model.js
+++ Models/User.Model.js
@@ -3,10 +3,14 @@ const { model, Schema } = mongoose;
 
 const userSchema = new Schema(
     {
-        fullName: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        phone: { type: String, required: true },
-        password: { type: String, required: true },
+        fullName: { type: String, required: [true, "{PATH} is required"] },
+        email: {
+            type: String,
+            required: [true, "{PATH} is required"],
+            unique: true,
+        },
+        phone: { type: String, required: [true, "{PATH} is required"] },
+        password: { type: String, required: [true, "{PATH} is required"] },
     },
     { timestamps: true }
 );
---
+++ utils/validationSchema.js
@@ -0,0 +1,29 @@
+import * as Yup from "yup";
+
+export const loginValidationSchema = Yup.object({
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Name is required")
+        .trim(),
+    password: Yup.string().required("Password is required").min(8),
+});
+
+export const signUpValidationSchema = Yup.object({
+    fullName: Yup.string().required("Name is required"),
+    phone: Yup.string().required("Phone Number is required"),
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Email is required"),
+    password: Yup.string()
+        .min(8, "Password must contain at least 8 characters")
+        .required("Password required"),
+});
+
+export const billValidationSchema = Yup.object({
+    fullName: Yup.string().required("Full Name is required"),
+    email: Yup.string()
+        .email("Enter a valid email")
+        .required("Email is required"),
+    phone: Yup.string().required("Phone Number is required"),
+    paidAmount: Yup.string().required("Paid Amount is required"),
+});
